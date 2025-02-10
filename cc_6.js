b2c3faff72ec1c23f6f64938cbabefb3bf82dfe2
Ashley Le <ashleyle@usf.edu>
2025-02-09 21:36:07 -0500

    Task 5 - Currency Conversion

--- cc_6.js
+++ cc_6.js
@@ -43,3 +43,11 @@ function calculateSubscriptionCost(plan, months, discount = 0) {
 };
 console.log(calculateSubscriptionCost("Basic", 6, 10)); // Expected output: "Total Cost: $50"
 console.log(calculateSubscriptionCost("Premium", 12, 0)); // Expected output: "Total Cost: $240"
+
+//Task 5: Returning Values
+function convertCurrency(amount, exchangeRate) {
+    let convertedAmount = amount * exchangeRate;
+    return `Converted Amount: $${convertedAmount}`; //labeling the converted amount
+};
+console.log(convertCurrency(100, 1.1)); // Expected output: "Converted Amount: $110.00"
+console.log(convertCurrency(250, 0.85)); // Expected output: "Converted Amount: $212.50"
