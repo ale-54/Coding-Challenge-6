5175ad6e1ebc0a9b5d6cd6e8a2d3f823d12af183
Ashley Le <ashleyle@usf.edu>
2025-02-09 21:09:49 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -5,3 +5,11 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
 }; 
 console.log(calculateProfit(20, 30, 100)); // Expected output: "Total Profit: $1000"
 console.log(calculateProfit(50, 70, 200)); // Expected output: "Total Profit: $4000"
+
+//Task 2: Function Expression
+function calculateSalesTax(amount, taxRate) {
+    let salesTax = amount * taxRate;
+    return `Sales Tax: $${salesTax}`;
+}; //calculating tax based on the amount
+console.log(calculateSalesTax(100, 0.07)); // Expected output: "Sales Tax: $7"
+console.log(calculateSalesTax(500, 0.1));  // Expected output: "Sales Tax: $50"
